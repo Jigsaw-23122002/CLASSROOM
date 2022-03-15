d63f24b9e0cc6f7eb407feef9f9c0c040b4fa322
Harsh <harshnag23@gmail.com>
2022-03-16 01:14:12 +0530

    Classroom

--- app/src/main/java/com/example/test_01/SignUp.java
+++ app/src/main/java/com/example/test_01/SignUp.java
@@ -2,6 +2,7 @@ package com.example.test_01;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -36,6 +37,7 @@ public class SignUp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_sign_up);
         LoginPageRedirectButton = (Button) findViewById(R.id.LoginPageRedirectButton);
         SignUpButton = (Button) findViewById(R.id.SignUpButton);
